a782a67fc9998c86bf92703873af09c7e2ca36c2
Lauris-altz git config --global user.name Lauris-altz <laura.altozano.2004@gmail.com>
2026-08-08 19:18:01 +0200

    change 6 PUF

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* HOME CAROUSEL */
+
 const slides = Array.from(document.querySelectorAll("[data-slide]"));
 const projectLabel = document.querySelector("[data-project-label]");
 let currentSlide = 0;
@@ -14,4 +16,24 @@ if (slides.length > 1) {
       projectLabel.textContent = slides[currentSlide].dataset.projectName || "";
     }
   }, 3000);
+}
+
+/* LA SOBREMESA MANUAL CAROUSEL */
+
+const manualCarousel = document.querySelector("[data-manual-carousel]");
+
+if (manualCarousel) {
+  const manualSlides = Array.from(
+    manualCarousel.querySelectorAll("[data-manual-slide]")
+  );
+
+  let currentManualSlide = 0;
+
+  if (manualSlides.length > 1) {
+    window.setInterval(() => {
+      manualSlides[currentManualSlide].classList.remove("is-active");
+      currentManualSlide = (currentManualSlide + 1) % manualSlides.length;
+      manualSlides[currentManualSlide].classList.add("is-active");
+    }, 3000);
+  }
 }
