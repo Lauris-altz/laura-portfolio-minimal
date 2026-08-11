cfb0c6569d3cfa3dd03e9c3673b8d37cefb9951b
Lauris-altz git config --global user.name Lauris-altz <laura.altozano.2004@gmail.com>
2026-08-11 23:30:24 +0200

    change 16-intro

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
 "use strict";
 
 
-/* =========================================
-   HOME CAROUSEL
-========================================= */
-
-const slides = Array.from(
-  document.querySelectorAll("[data-slide]")
-);
-
-const projectLabel =
-  document.querySelector("[data-project-label]");
-
-let currentSlide = 0;
-
-if (slides.length > 1) {
-
-  window.setInterval(() => {
-
-    slides[currentSlide]
-      .classList.remove("is-active");
-
-    currentSlide =
-      (currentSlide + 1) % slides.length;
-
-    slides[currentSlide]
-      .classList.add("is-active");
-
-
-    if (projectLabel) {
-
-      projectLabel.textContent =
-        slides[currentSlide].dataset.projectName || "";
-
-    }
-
-  }, 3000);
-
-}
 
 
 /* =========================================
